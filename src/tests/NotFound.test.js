9a0766cf2bdd65c5449cbbbb2bc744c9e2fac660
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2021-11-30 18:16:04 -0300

    feat:[requisito4] adicionou testeverificando se o componente NotFound.js possui um Heading h2 e se renderiza o emoji solicitado

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import { NotFound } from '../components';
+import renderWithRouter from './renderWithRouter';
+
+describe('4-Teste o componente <NotFound.js />', () => {
+  it('(A)-Teste se página contém um h2 com o "texto Page requested not found"', () => {
+    renderWithRouter(<NotFound />);
+    const text = screen.getByText('Page requested not found');
+    expect(text).toBeDefined();
+  });
+  it('(B)-Teste se página mostra o emoji', () => {
+    renderWithRouter(<NotFound />);
+    const image = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
