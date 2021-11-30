a09fc0b1b8062c4791559965c61394fe5827a267
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2021-11-30 16:10:27 -0300

    feat: [requisito1] adicionou testes dos links de navegacao

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('1 - Teste o componente <App.js />', () => {
+  it('(A)-Teste se o topo da aplicação contém um conjunto de links de navegação', () => {
+    renderWithRouter(<App />);
+    const home = screen.getByRole('link', { name: 'Home' });
+    const about = screen.getByRole('link', { name: 'About' });
+    const favoritePokemons = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+    expect(home).toBeDefined();
+    expect(about).toBeDefined();
+    expect(favoritePokemons).toBeDefined();
+  });
+});
