639a3fc0825026ae07bdb280088a9df2d77303ab
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2021-11-30 18:25:46 -0300

    feat:[requisito5] adicionou verificacao se renderiza o header H2

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste o componente <Pokedex.js />', () => {
+  it('(A)-Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
+    renderWithRouter(<App />);
+    const title = screen
+      .getByRole('heading', { level: 2, name: /Encountered pokémons/ });
+    expect(title).toBeDefined();
+  });
+
+});
