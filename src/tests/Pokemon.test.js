042202516739b426f1894b85ec1fee9af9a66f54
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2021-12-01 16:08:35 -0300

    feat:[requisito6] adicionou testes de renderizacao do card, redirecionamento de links e funcao de favoritar no pokemon.js

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,64 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Pokemon } from '../components';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testando o componente <Pokemon.js />', () => {
+  it('Teste se é renderizado um card com as informações de determinado pokémon.', () => {
+    const pokemon = {
+      id: 25,
+      name: 'Pikachu',
+      type: 'Electric',
+      averageWeight: {
+        value: '6.0',
+        measurementUnit: 'kg',
+      },
+      image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+      moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+      foundAt: [
+        {
+          location: 'Kanto Viridian Forest',
+          map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+        },
+        {
+          location: 'Kanto Power Plant',
+          map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+        },
+      ],
+      summary: 'This intelligent Pokémon ...',
+    };
+
+    renderWithRouter(<Pokemon pokemon={ pokemon } isFavorite />);
+    expect(screen.getByTestId('pokemon-name')).toHaveTextContent('Pikachu');
+    expect(screen.getByTestId('pokemon-type')).toHaveTextContent('Electric');
+    expect(screen.getByTestId('pokemon-weight'))
+      .toHaveTextContent('Average weight: 6.0 kg');
+    const img = screen.getByAltText('Pikachu sprite');
+    expect(img.src).toBe('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  });
+
+  it('Teste se o card do Pokémon indicado na Pokédex contém um link de navegação', () => {
+    const { history } = renderWithRouter(<App />);
+    const moreDetails = screen.getByRole('link', { name: 'More details' });
+
+    userEvent.click(moreDetails);
+    expect(history.location.pathname).toBe('/pokemons/25');
+    expect(moreDetails).toBeDefined();
+  });
+
+  it('Teste se existe um ícone de estrela nos Pokémons favoritados', () => {
+    renderWithRouter(<App />);
+    const moreDetails = screen.getByRole('link', { name: 'More details' });
+    userEvent.click(moreDetails);
+
+    const favoritedPokemon = screen
+      .getByRole('checkbox', { name: 'Pokémon favoritado?' });
+    userEvent.click(favoritedPokemon);
+
+    const img = screen.getByAltText('Pikachu is marked as favorite');
+
+    expect(img.src).toBe('http://localhost/star-icon.svg');
+  });
+});
